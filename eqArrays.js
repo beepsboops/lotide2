6e702b589f318ca46a3f85eb0958ca134856f553
A <awongpro7@gmail.com>
2021-03-30 23:12:14 +0000

    Adding file (eqArrays.js) for Implement eqArrays assignment

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+// ASSERT EQUAL FUNCTION
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return `😡 Assertion Failed: ${actual} !== ${expected}`;
+  } else if (actual === expected) {
+    return `👍 Assertion Passed: ${actual} === ${expected}`;
+  }
+};
+
+// EQARRAYS FUNCTION
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true; // Critial to make sure this is outside loop
+  }
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
