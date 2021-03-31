ae137035cd7c3b65d5463c8b508f3c2c452cb5dd
A <awongpro7@gmail.com>
2021-03-31 21:29:34 +0000

    Adding file (middle.js) for Implement middle assignment

--- eqArrays.js
+++ eqArrays.js
@@ -23,6 +23,8 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
+// TEST CODE
+
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
