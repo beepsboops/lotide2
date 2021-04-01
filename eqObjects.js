392a56c943d532a758d55c50bbf598db08235e0e
A <awongpro7@gmail.com>
2021-04-01 05:48:14 +0000

    Adding file for Implement eqObjects assignment

---
+++ eqObjects.js
@@ -0,0 +1,64 @@
+// ASSERT EQUAL FUNCTION
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return `😡 Assertion Failed: ${actual} !== ${expected}`;
+  } else if (actual === expected) {
+    return `👍 Assertion Passed: ${actual} === ${expected}`;
+  }
+};
+
+// EQARRAYS FUNCTION
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true; // Critial to make sure this is outside loop
+  }
+};
+
+// EQOBJECTS FUNCTION
+
+const eqObjects = function(object1, object2) {
+  const listOfKeys1 = Object.keys(object1);
+  const listOfKeys2 = Object.keys(object2);
+  if (listOfKeys1.length !== listOfKeys2.length) {
+    return false;
+  }
+  for (let key of listOfKeys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+    if (Array.isArray(value1) || Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else {
+      if (value1 !== value2) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
